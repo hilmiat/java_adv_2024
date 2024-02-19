92a465ebcfdcb0dd4a9125a89ac4df902562d389
hilmiat <hilmi.tawakal@gmail.com>
2024-02-19 09:42:59 +0700

    add yohoho..

--- SimpleJava/Hello.java
+++ SimpleJava/Hello.java
@@ -1,6 +1,6 @@
 package SimpleJava;
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world yohohoho.....");
     }
 }

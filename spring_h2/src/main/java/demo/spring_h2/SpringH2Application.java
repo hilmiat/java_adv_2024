1851dd5450f50aa19b0fec1ab50e2c825415294d
iniyunus <enunwaw@gmail.com>
2024-02-28 09:11:13 +0700

    spring_h2

---
+++ spring_h2/src/main/java/demo/spring_h2/SpringH2Application.java
@@ -0,0 +1,13 @@
+package demo.spring_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringH2Application.class, args);
+	}
+
+}
